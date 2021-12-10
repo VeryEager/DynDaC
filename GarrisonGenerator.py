e59f9e59eb8a776b72fdd9c1e8be95929d954e2e
Asher <stoutashe@myvuw.ac.nz>
2021-12-10 21:53:51 +1300

    Garrison generation now uses probabilities formally

--- GarrisonGenerator.py
+++ GarrisonGenerator.py
@@ -46,25 +46,26 @@ class GarrisonGenerator:
         points = self.pts_def + (self.pts_per * tier_num)
         garrison = []
 
+        # Pre-empt garrison with a general
+        general = []
+        if rand.random() < self.prob_gen[tier_num]:
+            general.append(rand.choice(allowed[3], 1)[0])
+            points -= self.costs[2]  # use same cost as a rare
+        garrison.extend(general)
+
         # First add Rares
         rares = []
-        for i in range(self.max_rare[tier_num]):
-            if rand.random() > 0.85 or points < self.costs[2]:  # Some degree of uncertainty; the max allowed shouldn't always be used
-                break
-            else:
-                rares.append(rand.choice(allowed[2], 1)[0])
-                points -= self.costs[2]
+        while rand.random() < self.prob_rare[tier_num] and not points < self.costs[2]:
+            rares.append(rand.choice(allowed[2], 1)[0])
+            points -= self.costs[2]
         rares = sorted(rares)
         garrison.extend(rares)
 
         # Then add Uncommons
         unc = []
-        for i in range(self.max_uncommon[tier_num]):
-            if rand.random() > 0.95 or points < self.costs[1]:  # Some degree of uncertainty; the max allowed shouldn't always be used
-                break
-            else:
-                unc.append(rand.choice(allowed[1], 1)[0])
-                points -= self.costs[1]
+        while rand.random() < self.prob_rare[tier_num] and not points < self.costs[1]:
+            unc.append(rand.choice(allowed[1], 1)[0])
+            points -= self.costs[1]
         unc = sorted(unc)
         garrison.extend(unc)
 
@@ -78,11 +79,12 @@ class GarrisonGenerator:
         return garrison
 
     def __init__(self):
-        self.pts_def = 6  # starting capacity at villages
-        self.pts_per = 2  # additional points per settlement level
+        self.pts_def = 5  # starting capacity at villages
+        self.pts_per = 3  # additional points per settlement level
         self.costs = [1, 2, 3]  # costs for each unit rank
-        self.max_uncommon = [1, 1, 2, 3, 4, 5]  # for village, town, large town, city, large city, huge city
-        self.max_rare = [0, 0, 0, 1, 2, 3]
+        self.prob_uncommon = [0.25, 0.5, 0.75, 0.85, 0.9, 0.95]  # for village, town, large town, city, large city, huge city
+        self.prob_rare = [0.1, 0.3, 0.5, 0.7, 0.85, 0.9]
+        self.prob_gen = [0.2, 0.4, 0.6, 0.8, 0.9, 1.0]
         self.facnames = []  # loaded in __load_templates_()
         self.templates = []
         self.CITY_TIERS = ['village', 'town', 'large_town', 'city', 'large_city', 'huge_city']
